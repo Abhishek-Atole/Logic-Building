58805214473b8b29cc778dbe103c051923dd5806
Abhishek-Atole <abhiatole17@gmail.com>
2025-03-29 18:42:06 +0530

    Dynamic Loop (Loop Body gets changed)

--- program37.c
+++ program37.c
@@ -4,7 +4,7 @@ void Display()
     int iCnt = 0;
     for (iCnt = 1; iCnt <= 5; iCnt++)
     {
-        printf("Hello %d\n",iCnt);
+        printf("Hello %d\n",iCnt);     //Dynamic loop (loop body gets changed)
     }
 }
 int main()
