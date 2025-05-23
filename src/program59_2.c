900bb1778172b8c60f0a7dee5917086fb85f61a8
Abhishek-Atole <abhiatole17@gmail.com>
2025-05-23 14:42:20 +0530

    Programs on Digits

---
+++ src/program59_2.c
@@ -0,0 +1,35 @@
+// Form Here Problems on Digits started from Here
+#include <stdio.h>
+
+int CountDigits(int iNo)
+{
+    int iCnt = 0;
+    int iDigit =0;
+    // Joparyanta iNo  0 hot
+    // Nahi Toparyanta
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iCnt++;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    auto int iValue = 0;
+    auto int iRet =0 ;
+    printf("Enter the Number : ");
+    scanf("%d", &iValue);
+    iRet = CountDigits(iValue);
+    printf("No of digits Are : %d", iRet);
+
+    return 0;
+}
+
+/*
+
+    Note : In this Program While loop id used because we kmow we have to iterate but we
+            dont know how many times but we know at which condition we have to stop
+
+*/
---
+++ src/program61.c
@@ -0,0 +1,40 @@
+// Form Here Problems on Digits started from Here
+#include <stdio.h>
+
+int SumofDigits(int iNo)
+{
+    int iSum =0;
+    int iDigit = 0;
+    // Joparyanta iNo  0 hot
+    // Nahi Toparyanta
+    while (iNo != 0)
+    {
+        
+        iSum = iSum + (iNo % 10);
+        printf("Isum is : %d \n: ",iSum);
+        iNo = iNo / 10;
+        printf("Ino is  : %d \n",iNo);     
+        
+
+    }
+    return iSum;
+}
+
+int main()
+{
+    auto int iValue = 0;
+    auto int iRet =0 ;
+    printf("Enter the Number : ");
+    scanf("%d", &iValue);
+    iRet = SumofDigits(iValue);
+    printf("Sum of digits Are : %d", iRet);
+
+    return 0;
+}
+
+/*
+
+    Note : In this Program While loop id used because we kmow we have to iterate but we
+            dont know how many times but we know at which condition we have to stop
+
+*/
