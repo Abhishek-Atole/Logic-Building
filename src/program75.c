351534e5f5ecae3b49601a4bc0ce165fbd795717
Abhishek-Atole <abhiatole17@gmail.com>
2025-05-26 18:41:53 +0530

    Addition of the array

---
+++ src/program75.c
@@ -0,0 +1,47 @@
+// Accept N numbers form user and print the Addition of an array
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int Addition(int Arr[],int iSize)
+{
+    int iCnt = 0;
+    int iSum =0;
+
+    for (iCnt = 0;iCnt <iSize;iCnt++)
+    {
+        iSum =iSum + Arr[iCnt];
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iCount = 0;
+    int *ptr = NULL;
+    int iCnt = 0;
+    int iRet = 0;
+
+    printf("Enter the Number of Elements : ");
+    scanf("%d", &iCount);
+
+    ptr = (int *)malloc(iCount * sizeof(int));
+    printf("Dynamic Memory Allocated Succesfully for %d elements.\n", iCount);
+
+    printf("Enter the %d Elements.\n", iCount);
+
+    for (iCnt = 0; iCnt < iCount; iCnt++)
+    {
+        printf("Enter the Value of for Index %d : ", iCnt);
+        scanf("%d", &ptr[iCnt]);
+    }
+
+    iRet = Addition(ptr,iCount);
+    
+    printf("The Addition of the Array is %d.\n",iRet);
+
+    free(ptr);
+    printf("Dynamic Memory Deallocates Successfully.");
+
+    return 0;
+}
