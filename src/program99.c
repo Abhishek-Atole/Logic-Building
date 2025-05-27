daad6e06adf05ec2682673b003aa55dd3103d0ee
Abhishek-Atole <abhiatole17@gmail.com>
2025-05-27 22:33:34 +0530

    // Pattern Printing // iRow = 3 // iColumn = 4 /*     1   1   1   1     1   1   1   1     1   1   1   1 */

---
+++ src/program99.c
@@ -0,0 +1,38 @@
+// Pattern Printing
+// iRow = 3
+// iColumn = 4
+/*
+    1   1   1   1
+    1   1   1   1
+    1   1   1   1
+*/
+
+#include <stdio.h>
+
+void Display(int iRow, int iColumn)
+{
+    {
+        int i, j = 0;
+        for (i = 0; i < iRow; i++)
+        {
+            for (j = 0; j < iColumn; j++)
+            {
+                printf("1\t");
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main()
+{
+    int iNo1 = 0;
+    int iNo2 = 0;
+    printf("Enter the Number of Row : ");
+    scanf("%d", &iNo1);
+    printf("Enter the Number of Column : ");
+    scanf("%d", &iNo2);
+
+    Display(iNo1, iNo2);
+    return 0;
+}
