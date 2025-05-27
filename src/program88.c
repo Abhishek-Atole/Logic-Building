c0fcc8fce7749d75d3600e7665a14f305621845a
Abhishek-Atole <abhiatole17@gmail.com>
2025-05-27 17:11:45 +0530

    Printing the Number Upto the input form o and from 1 to N number

---
+++ src/program88.c
@@ -0,0 +1,25 @@
+
+//Printing this pAttern on the screen 
+// 1    2   3   4   5   6
+// *    *   *   *   *   *
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt =0;
+    for (iCnt =0 ;iCnt <= iNo;iCnt++)
+    {
+        printf("%d\t",iCnt);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+    printf("Enter the Count : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+    return 0;
+}
