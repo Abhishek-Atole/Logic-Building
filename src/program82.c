96314512a594df51476e0b7620d59355f7e7c62e
Abhishek-Atole <abhiatole17@gmail.com>
2025-05-27 14:05:20 +0530

    Both Maximum and mimimum values

---
+++ src/program82.c
@@ -0,0 +1,48 @@
+// Program to Choose the Maximum or minumum Nuber form the Array
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void MaximumMinimum(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iMax = Arr[0];
+    int iMin = Arr[0];
+
+    for (iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if (Arr[iCnt] > iMax)
+        {
+            iMax = Arr[iCnt];
+        }
+        if (Arr[iCnt] < iMin)
+        {
+            iMin = Arr[iCnt];
+        }
+    }
+    printf("The Maximum Value is : %d\n", iMax);
+    printf("The Minimum Value is : %d\n", iMin);
+}
+
+int main()
+{
+    int iCnt = 0;
+    int iCount = 0;
+    int *ptr = NULL;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &iCount);
+
+    ptr = (int *)malloc(iCount * sizeof(int));
+    printf("Dynamic Memory allocated successfully for the % d numbers.\n", iCount);
+
+    for (iCnt = 0; iCnt < iCount; iCnt++)
+    {
+        printf("Enter the Numbers for index No %d : \n", iCnt);
+        scanf("%D", &ptr[iCnt]);
+    }
+    MaximumMinimum(ptr, iCount);
+    free(ptr);
+    printf("Dynamic Memory Deallocates Successfully.\n");
+    return 0;
+}
