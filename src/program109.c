1bee5d89d22ad6c47b43c90e63d646d64e8822a1
Abhishek-Atole <abhiatole17@gmail.com>
2025-05-28 13:55:59 +0530

    Pattern Operations Column

---
+++ src/program109.c
@@ -0,0 +1,45 @@
+// Pattern Printing
+// iRow = 3
+// iColumn = 4
+/*
+    1   1   1   1
+    1   1   1   1
+    1   1   1   1
+*/
+
+#include <stdio.h>
+
+void Display(int iRow, int iColumn)
+{
+    {
+        int i, j = 0;
+        for (i = 1; i <= iRow; i++)
+        {
+            for (j = 1; j <= iColumn; j++)
+            {
+                if (i == j)
+                {
+                    printf("*\t");                    
+                }
+                else
+                {
+                    printf("%d\t",j);
+                }
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main()
+{
+    int iNo1 = 0;
+    int iNo2 = 0;
+    printf("Enter the Number of Row : ");
+    scanf("%d", &iNo1);
+    printf("Enter the Number of Column : ");
+    scanf("%d", &iNo2);
+
+    Display(iNo1, iNo2);
+    return 0;
+}
