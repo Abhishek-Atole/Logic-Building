cac8d66df0c5989b7b0fcd2e1b7b69b5df1ac8d0
Abhishek-Atole <abhiatole17@gmail.com>
2025-05-28 20:47:02 +0530

    Check Wheather teh Letter is capital or not

---
+++ src/program119.c
@@ -0,0 +1,33 @@
+// Program to calculate the Character is capital or Small
+#include <stdio.h>
+#include <stdbool.h>
+
+bool Capital(char Cvalue)
+{
+    if ((Cvalue >= 'A') && (Cvalue <= 'Z'))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    char Ch = '\0';
+    bool iRet = false;
+    printf("Enter the Character : ");
+    scanf("%c", &Ch);
+    iRet = Capital(Ch);
+    if (iRet == true)
+    {
+        printf("%c is the Capital Letter.", Ch);
+    }
+    else
+    {
+        printf("%c is Small Letter.", Ch);
+    }
+    return 0;
+}
