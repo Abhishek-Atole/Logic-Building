0566e2a6017f72bcabe1a0721615fc78169228ac
Abhishek-Atole <abhiatole17@gmail.com>
2025-05-29 19:38:27 +0530

    String Convert Capital to small , Small to Capital

---
+++ src/program141.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void Strlwr(char str[]) // Here the array is taken as a input to this function
+{
+    int iCnt = 0;
+
+    while (*str != '\0')
+    {
+
+        if ((*str >= 'A') && (*str <= 'Z'))
+        {
+            *str = *str + 32;
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[20]; // Array to store the input string (max 19 characters + null terminator '\0')
+    // Prompt the user to enter a string
+    printf("Enter the String : \n");
+    scanf("%[^'\n']s", Arr);
+    Strlwr(Arr);
+    printf("Updated String is :  %s", Arr);
+    return 0;
+}
---
+++ src/program144.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void Toggle(char str[]) // Here the array is taken as a input to this function
+{
+    int iCnt = 0;
+
+    while (*str != '\0')
+    {
+
+        if ((*str >= 'a') && (*str <= 'z'))
+        {
+            *str = *str - 32;
+        }
+        else if ((*str >= 'A') && (*str <= 'Z'))
+        {
+            *str = *str + 32;
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[20]; // Array to store the input string (max 19 characters + null terminator '\0')
+    // Prompt the user to enter a string
+    printf("Enter the String : \n");
+    scanf("%[^'\n']s", Arr);
+    Toggle(Arr);
+    printf("Updated String is :  %s", Arr);
+    return 0;
+}
+
+// In this Type of Code We  DirectlyEdit the data by acceing theri memory and  edit and store it . so we dont use return
