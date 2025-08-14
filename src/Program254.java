15c819cec0e45b7523064138b7d9b8dd4ad71919
Abhiatole <your-email@example.com>
2025-08-14 19:33:14 +0530

    feat: add Java basic string input and display

---
+++ src/Program254.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Program254 {
+    public static void main(String[] args) {
+        Scanner sobj = new Scanner(System.in);
+
+        String str = null;
+
+        System.out.print("Enter Your name : ");
+        str = sobj.nextLine();
+
+        System.out.println("Your name is : " + str);
+        sobj.close();
+
+    }
+
+}
