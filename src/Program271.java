6161a457bb5cdf552c53cc16c2ae5d33c8468587
Abhiatole <your-email@example.com>
2025-08-18 03:46:16 +0530

    feat: add 9th bit checker using hexadecimal notation

---
+++ src/Program271.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Program271 {
+
+    public static boolean CheckBit(int iNo) {
+        int iMask = 0x00000100;
+        int iResult = 0;
+        iResult = iNo & iMask;
+        if (iResult == iMask) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sobj = new Scanner(System.in);
+        int iNo = 0;
+        boolean bRet = false;
+
+        System.out.println("Enter the No : ");
+        iNo = sobj.nextInt();
+
+        bRet = CheckBit(iNo);
+
+        if (bRet == true) {
+            System.out.println("3rd and 9th Bit is ON");
+        } else {
+            System.out.println("3rd and 9th bit is OFF");
+        }
+        sobj.close();
+
+    }
+
+}
